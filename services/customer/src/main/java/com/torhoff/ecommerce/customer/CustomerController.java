d99d9888deda44a74e8acf69af86ad5ecfda78ef
Aleks Torhov <arcturus1986@abv.bg>
2024-12-26 14:27:12 +0200

    test - services restored

---
+++ services/customer/src/main/java/com/torhoff/ecommerce/customer/CustomerController.java
@@ -0,0 +1,62 @@
+package com.torhoff.ecommerce.customer;
+
+import jakarta.validation.Valid;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/customer")
+@RequiredArgsConstructor
+public class CustomerController {
+
+    @Autowired
+    private final CustomerService customerService;
+
+    @PostMapping
+    public ResponseEntity<String> createCustomer(
+            @RequestBody @Valid CustomerRequest request
+    ) {
+        return ResponseEntity.ok(customerService.createCustomer(request));
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> updateCustomer(
+            @RequestBody @Valid CustomerRequest request
+    ) {
+        customerService.updateCustomer(request);
+        return ResponseEntity.accepted().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CustomerResponse>> findAll() {
+        return ResponseEntity.ok(customerService.findAllCustomers());
+    }
+
+    @GetMapping("/exists/{customer-id}")
+    public ResponseEntity<Boolean> existsById(
+            @PathVariable("customer-id") String customerId
+    ) {
+        return ResponseEntity.ok(customerService.existsById(customerId));
+    }
+
+    @GetMapping("/{customer-id}")
+    public ResponseEntity<CustomerResponse> findById(
+            @PathVariable("customer-id") String customerId
+    ) {
+        return ResponseEntity.ok(customerService.findById(customerId));
+    }
+
+    @DeleteMapping("/{customer-id}")
+    public ResponseEntity<Void> deleteById(
+            @PathVariable("customer-id") String customerId
+    ) {
+        customerService.deleteCustomer(customerId);
+        return ResponseEntity.accepted().build();
+    }
+
+}
