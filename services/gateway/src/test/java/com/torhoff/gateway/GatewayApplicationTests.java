d999de7a98eca79d8e9560d678ebc67a20fc36cb
Aleks Torhov <arcturus1986@abv.bg>
2025-01-03 12:37:26 +0200

    Gateway implemented

---
+++ services/gateway/src/test/java/com/torhoff/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.torhoff.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
